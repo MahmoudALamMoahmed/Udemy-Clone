c378a4bb5ba3ff2bb1e6dfc247b791295afd6844
MahmoudALamMoahmed <mahmoudalm2019@gmail.com>
2025-04-27 19:59:24 +0300

    EditPort

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,3 @@
-// const jsonServer = require('json-server')
-// const multer  = require('multer')
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
-// const middlewares = jsonServer.defaults()
-
-// // Set default middlewares (logger, static, cors and no-cache)
-// server.use(middlewares)
-
-// // Add custom routes before JSON Server router
-// /* server.get('/echo', (req, res) => {
-//   res.jsonp(req.query)
-// }) */
-
-//   const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, 'public/images')
-//     },
-//     filename: function (req, file, cb) {
-//       let date=new Date()
-//       let url = date.getTime()+"_"+file.originalname
-//       req.body.url=url
-//       cb(null, url)
-//     }
-//   })
-
-//   const bodyParser = multer({ storage: storage }).any()
-
-// // To handle POST, PUT and PATCH you need to use a body-parser
-// // You can use the one used by JSON Server
-// server.use(bodyParser)
-// server.post("/products",(req, res, next) => {
-// let date = new Date()
-// req.body.createdAt = date.toISOString()
-
-// if(req.body.price){
-//   req.body.price=Number(req.body.price)
-// }
-
-// let hasErrors = false
-// let errors={}
-// if(req.body.title.length<2){
-//   hasErrors = true
-//   errors.title="the title length should be at least 2 characters"
-// }
-// if(req.body.price.length<=0){
-//   hasErrors = true
-//   errors.price="the price is not valid"
-// }
-
-// if(hasErrors){
-//   //return bad req 400 with validation errors
-//   res.status(400).jsonp(errors)
-//   return
-// }
-//   // Continue to JSON Server router
-//   next()
-// })
-
-// // Use default router
-// server.use(router)
-// server.listen(4000, () => {
-//   console.log('JSON Server is running')
-// })
-
 // server.js
 require("dotenv").config();
 const jsonServer = require("json-server");
@@ -199,6 +134,8 @@ server.delete("/products/:id", async (req, res, next) => {
 server.use(router);
 
 // تشغيل السيرفر
-server.listen(4000, () => {
-  console.log("✅ JSON Server with Cloudinary is running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`✅ JSON Server with Cloudinary is running on port ${PORT}`);
 });
