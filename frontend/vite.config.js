695462a42743ab997efb1e2a437d6ec4a7752cc7
MahmoudALamMoahmed <mahmoudalm2019@gmail.com>
2025-04-28 07:01:59 +0300

    Edit in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // يسمع على كل الشبكات مش localhost بس
+    port: import.meta.env.VITE_PORT || 5173, // يستخدم البورت اللي Render يديه، أو 5173 لو بتشغل محلي
+  },
+});
