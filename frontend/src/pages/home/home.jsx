1aeb46f894fad31bf17a4a3d9c2cffc015792fb0
MahmoudALamMoahmed <mahmoudalm2019@gmail.com>
2025-04-28 23:35:40 +0300

    Debugs

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -51,24 +51,53 @@ export function HomePage() {
             Trusted by over 15,000 companies and millions of learners around the world
           </h3>
           <div className="d-flex justify-content-between align-items-center gap-5 mt-5 flex-wrap">
-            {/* صور الشعارات */}
-            {[ 
-              "volkswagen_logo.svg",
-              "samsung_logo.svg",
-              "cisco_logo.svg",
-              "vimeo_logo_resized-2.svg",
-              "procter_gamble_logo.svg",
-              "hewlett_packard_enterprise_logo.svg",
-              "citi_logo.svg",
-              "ericsson_logo.svg",
-            ].map((logo, index) => (
-              <img
-                key={index}
-                src={`https://cms-images.udemycdn.com/content/${logo}?position=c&quality=80&x.app=portals`}
-                alt={`${logo.split("_")[0]} logo`}
-                loading="lazy"
-              />
-            ))}
+            <img
+              src="https://cms-images.udemycdn.com/content/tqevknj7om/svg/volkswagen_logo.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Volkswagen logo gray and white logo"
+              loading="lazy"
+            />
+
+            <img
+              src="https://cms-images.udemycdn.com/content/2gevcc0kxt/svg/samsung_logo.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Samsung logo gray and white logo"
+              loading="lazy"
+            />
+
+            <img
+              src="https://cms-images.udemycdn.com/content/mueb2ve09x/svg/cisco_logo.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Cisco logo gray and white logo"
+              loading="lazy"
+            />
+
+            <img
+              src="https://cms-images.udemycdn.com/content/ryaowrcjb2/svg/vimeo_logo_resized-2.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Vimeo logo gray and white logo"
+              loading="lazy"
+            />
+
+            <img
+              src="https://cms-images.udemycdn.com/content/bthyo156te/svg/procter_gamble_logo.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Procter &amp; Gamble logo gray and white logo"
+              loading="lazy"
+            />
+
+            <img
+              src="https://cms-images.udemycdn.com/content/luqe0d6mx2/svg/hewlett_packard_enterprise_logo.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Hewlett Packard logo gray and white logo"
+              loading="lazy"
+            />
+
+            <img
+              src="https://cms-images.udemycdn.com/content/siaewwmkch/svg/citi_logo.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Citi logo gray and white logo"
+              loading="lazy"
+            />
+
+            <img
+              src="https://cms-images.udemycdn.com/content/swmv0okrlh/svg/ericsson_logo.svg?position=c&amp;quality=80&amp;x.app=portals"
+              alt="Ericsson logo gray and white logo"
+              loading="lazy"
+            />
           </div>
         </div>
       </div>
